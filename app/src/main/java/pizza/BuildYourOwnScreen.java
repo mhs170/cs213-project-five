2fb484263fc84d0eeb143a430483a7306ab2fa35
Dakshal P <dakshal29@gmail.com>
2023-12-04 23:01:54 -0500

    add recyclerview to speciality pizza screen

--- app/src/main/java/pizza/BuildYourOwnScreen.java
+++ app/src/main/java/pizza/BuildYourOwnScreen.java
@@ -8,35 +8,58 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import com.example.cs213_project_five.R;
 import com.example.cs213_project_five.databinding.ActivityBuildYourOwnScreenBinding;
 
+import java.util.ArrayList;
+
 public class BuildYourOwnScreen extends AppCompatActivity {
 
-    private ActivityBuildYourOwnScreenBinding binding;
+    private ArrayList<Item> items = new ArrayList<>();
 
+    private int [] itemImages = {
+            R.drawable.deluxe, R.drawable.meatzza,
+            R.drawable.pepperoni, R.drawable.seafood, R.drawable.supreme
+    };
+
+    /**
+     * Get the references of all instances of Views defined in the layout file, set up the list of
+     * items to be display in the RecyclerView.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_your_own_screen);
+        RecyclerView rcview = findViewById(R.id.rcView_menu);
+        setupMenuItems(); //add the list of items to the ArrayList
+        ItemsAdapter adapter = new ItemsAdapter(this, items); //create the adapter
+        rcview.setAdapter(adapter); //bind the list of items to the RecyclerView
+        //use the LinearLayout for the RecyclerView
+        rcview.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-        binding = ActivityBuildYourOwnScreenBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    /**
+     * Helper method to set up the data (the Model of the MVC).
+     */
+    private void setupMenuItems() {
+        /*
+         * Item names are defined in a String array under res/string.xml.
+         * Your item names might come from other places, such as an external file, or the database
+         * from the backend.
+         */
+        String [] itemNames = getResources().getStringArray(R.array.itemNames);
+        /* Add the items to the ArrayList.
+         * Note that I use hardcoded prices for demo purpose. This should be replace by other
+         * data sources.
+         */
+        for (int i = 0; i < itemNames.length; i++) {
+            items.add(new Item(itemNames[i], itemImages[i], "$1.39"));
+        }
     }
 }
--- app/src/main/java/pizza/SpecialtyPizzaScreen.java
+++ app/src/main/java/pizza/SpecialtyPizzaScreen.java
@@ -2,39 +2,63 @@ package pizza;
 
 import android.os.Bundle;
 
+import com.example.cs213_project_five.R;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import com.example.cs213_project_five.databinding.ActivitySpecialtyPizzaScreenBinding;
 
+import java.util.ArrayList;
+
 public class SpecialtyPizzaScreen extends AppCompatActivity {
+    private ArrayList<Item> items = new ArrayList<>();
 
-    private ActivitySpecialtyPizzaScreenBinding binding;
+    private int [] itemImages = {
+            R.drawable.deluxe, R.drawable.meatzza,
+            R.drawable.pepperoni, R.drawable.seafood, R.drawable.supreme
+    };
 
+    /**
+     * Get the references of all instances of Views defined in the layout file, set up the list of
+     * items to be display in the RecyclerView.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specialty_pizza_screen);
+        RecyclerView rcview = findViewById(R.id.rcView_menu);
+        setupMenuItems(); //add the list of items to the ArrayList
+        ItemsAdapter adapter = new ItemsAdapter(this, items); //create the adapter
+        rcview.setAdapter(adapter); //bind the list of items to the RecyclerView
+        //use the LinearLayout for the RecyclerView
+        rcview.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-        binding = ActivitySpecialtyPizzaScreenBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+    /**
+     * Helper method to set up the data (the Model of the MVC).
+     */
+    private void setupMenuItems() {
+        /*
+         * Item names are defined in a String array under res/string.xml.
+         * Your item names might come from other places, such as an external file, or the database
+         * from the backend.
+         */
+        String [] itemNames = getResources().getStringArray(R.array.itemNames);
+        /* Add the items to the ArrayList.
+         * Note that I use hardcoded prices for demo purpose. This should be replace by other
+         * data sources.
+         */
+        for (int i = 0; i < itemNames.length; i++) {
+            items.add(new Item(itemNames[i], itemImages[i], "$1.39"));
+        }
     }
 }
